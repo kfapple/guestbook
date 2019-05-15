7fe92c1e22aca6b7d38b131d7bcffa1d511b207b
kfapple <365goodlife@gmail.com>
2019-05-15 15:59:29 +0800

    2

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'message',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'message',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 登入後重新導向首頁 (預設會導向 /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
